fa649c0bc6e468d44247a23b2d97b9651ffa7098
yangyang <charlesmaxwellyoung@gmail.com>
2026-03-08 22:39:34 +0800

    feat(photos): 添加多张新照片资源
    - 新增照片资源，包括 IMG_0192.webp、IMG_0213.webp、IMG_0225.webp 等
    - 更新 photos.ts 文件以引用新添加的照片

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -1,4 +1,16 @@
 import type { PhotoData } from '~/types'
+import img0192 from '~/assets/photos/IMG_0192.webp'
+import img0213 from '~/assets/photos/IMG_0213.webp'
+import img0225 from '~/assets/photos/IMG_0225.webp'
+import img0245 from '~/assets/photos/IMG_0245.webp'
+import img0246 from '~/assets/photos/IMG_0246.webp'
+import img0248 from '~/assets/photos/IMG_0248.webp'
+import img0255 from '~/assets/photos/IMG_0255.webp'
+import img0256 from '~/assets/photos/IMG_0256.webp'
+import img0257 from '~/assets/photos/IMG_0257.webp'
+import img0258 from '~/assets/photos/IMG_0258.webp'
+import img0285 from '~/assets/photos/IMG_0285.webp'
+import img0339 from '~/assets/photos/IMG_0339.webp'
 
 export const PhotosList: PhotoData[] = [
   {
@@ -11,84 +23,84 @@ export const PhotosList: PhotoData[] = [
     date: '2026-03',
     photos: [
       {
-        src: '/content/photos/IMG_0192.webp',
+        src: img0192,
         alt: '照片 IMG_0192',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0213.webp',
+        src: img0213,
         alt: '照片 IMG_0213',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0225.webp',
+        src: img0225,
         alt: '照片 IMG_0225',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0245.webp',
+        src: img0245,
         alt: '照片 IMG_0245',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0246.webp',
+        src: img0246,
         alt: '照片 IMG_0246',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0248.webp',
+        src: img0248,
         alt: '照片 IMG_0248',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0255.webp',
+        src: img0255,
         alt: '照片 IMG_0255',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0256.webp',
+        src: img0256,
         alt: '照片 IMG_0256',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0257.webp',
+        src: img0257,
         alt: '照片 IMG_0257',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0258.webp',
+        src: img0258,
         alt: '照片 IMG_0258',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0285.webp',
+        src: img0285,
         alt: '照片 IMG_0285',
         width: 1920,
         height: 1440,
         variant: '4x3',
       },
       {
-        src: '/content/photos/IMG_0339.webp',
+        src: img0339,
         alt: '照片 IMG_0339',
         width: 1920,
         height: 1440,
