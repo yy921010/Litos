2fc76a88bab90e1ede59e6b428d22507af03c5a0
yangyang <charlesmaxwellyoung@gmail.com>
2026-03-08 22:24:33 +0800

    feat(photos): 添加照片分享功能
    - 新增照片列表，包含多张照片的详细信息
    - 每张照片包含标题、描述、尺寸和路径
    
    Closes #123

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -1,3 +1,99 @@
 import type { PhotoData } from '~/types'
 
-export const PhotosList: PhotoData[] = []
+export const PhotosList: PhotoData[] = [
+  {
+    title: '摄影集锦',
+    icon: {
+      type: 'emoji',
+      value: '📷',
+    },
+    description: '记录生活中的美好瞬间',
+    date: '2026-03',
+    photos: [
+      {
+        src: '/content/photos/IMG_0192.webp',
+        alt: '照片 IMG_0192',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0213.webp',
+        alt: '照片 IMG_0213',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0225.webp',
+        alt: '照片 IMG_0225',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0245.webp',
+        alt: '照片 IMG_0245',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0246.webp',
+        alt: '照片 IMG_0246',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0248.webp',
+        alt: '照片 IMG_0248',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0255.webp',
+        alt: '照片 IMG_0255',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0256.webp',
+        alt: '照片 IMG_0256',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0257.webp',
+        alt: '照片 IMG_0257',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0258.webp',
+        alt: '照片 IMG_0258',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0285.webp',
+        alt: '照片 IMG_0285',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+      {
+        src: '/content/photos/IMG_0339.webp',
+        alt: '照片 IMG_0339',
+        width: 1920,
+        height: 1440,
+        variant: '4x3',
+      },
+    ],
+  },
+]
